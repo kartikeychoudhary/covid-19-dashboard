33b5c3457f7ac9e5a7254bd835e799a66c369db2
31shavy@gmail.com <31shavy@gmail.com>
2020-04-28 21:57:31 +0530

    Updates

---
+++ precache-manifest.ed0fdf1ad113766c949691b6b115b50d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287547e68f44d4c2de4ef082fd65efec",
+    "url": "/covid-19-dashboard/index.html"
+  },
+  {
+    "revision": "310b736fc43bde7fc82e",
+    "url": "/covid-19-dashboard/static/css/main.2a05285d.chunk.css"
+  },
+  {
+    "revision": "6e3f95f9f0221b822949",
+    "url": "/covid-19-dashboard/static/js/2.db879cb2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-dashboard/static/js/2.db879cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "310b736fc43bde7fc82e",
+    "url": "/covid-19-dashboard/static/js/main.b61975f7.chunk.js"
+  },
+  {
+    "revision": "89f28ba390961e66c4bc",
+    "url": "/covid-19-dashboard/static/js/runtime-main.612bc006.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-dashboard/static/media/image.d7265326.png"
+  }
+]);
